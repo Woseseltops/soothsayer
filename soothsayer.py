679878f870981b53ba958f3f9d4b5941af838a4e
Wessel Stoop <woseseltops@hotmail.com>
2013-02-01 16:51:25 +0100

    Added TiMBL preprocessing

--- soothsayer.py
+++ soothsayer.py
@@ -89,9 +89,51 @@ def demo_mode():
         if 'quit' in text_so_far.split():
             busy = False;
 
+def prepare_training_data(directory):
+
+    files = os.listdir(directory);
+    total_text = '';
+
+    for i in files:
+        total_text += ' _ _ _ '+open(directory+i,'r').read();
+
+    ngrams = window_string(total_text.strip())
+    training_file_content = '\n'.join(ngrams);
+    training_file = open('training.txt','w');
+    training_file.write(training_file_content);
+    
+    return training_file;
+    
+def window_string(string):
+    """Return the string as a list of 4-grams""";
+
+    words = string.split();
+    ngrams = [];
+    
+    for i in range(0,len(words)-3):
+        ngrams.append(' '.join(words[i:i+4]));
+
+    #Remove useless ones
+    ngrams_to_remove = [];
+    for i in ngrams:
+        if i[-1] == '_':
+            ngrams_to_remove.append(i);
+
+    for i in ngrams_to_remove:
+        ngrams.remove(i);
+
+    return ngrams;
+
 #########
 
 mode = input('Mode (d = demo, s = simulation): ');
+inp = input('Input directory (include (back)slash): ');
+
+f = prepare_training_data(inp);
 
 if mode == 'd':
     demo_mode();
+
+#Later
+# Server modus
+# Letter approach
