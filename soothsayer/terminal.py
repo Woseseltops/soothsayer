7e7107f98373af4ca37ad4332f285ff3ccb6bba1
Wessel Stoop <wstoop@fancypants.science.ru.nl>
2013-05-18 20:42:42 +0200

    Fixed colors

--- soothsayer/terminal.py
+++ soothsayer/terminal.py
@@ -33,8 +33,8 @@ def clear():
 
     os.system(['clear','cls'][os.name == 'nt'])
 
-colors = {'PERS. MODEL/IGTREE': '\033[035m', 'PERS. MODEL/LEXICON': '\033[134m',
-          'GEN. MODEL/IGTREE': '\033[032m', 'GEN. MODEL/LEXICON': '\033[131m',
-          'RECENCY BUFFER': '\033[131m', 'white': '\033[0m', 'I GIVE UP': '\033[0m',
-          'USER': '\033[0m', 'PERS. MODEL/IGTREE +RB': '\033[131m',
-          'GEN. MODEL/IGTREE +RB': '\033[131m'}
+colors = {'PERS. MODEL/IGTREE': '\033[0;35m', 'PERS. MODEL/LEXICON': '\033[1;34m',
+          'GEN. MODEL/IGTREE': '\033[0;32m', 'GEN. MODEL/LEXICON': '\033[1;31m',
+          'RECENCY BUFFER': '\033[1;31m', 'white': '\033[0m', 'I GIVE UP': '\033[0m',
+          'USER': '\033[0m', 'PERS. MODEL/IGTREE +RB': '\033[1;31m',
+          'GEN. MODEL/IGTREE +RB': '\033[1;31m'};
