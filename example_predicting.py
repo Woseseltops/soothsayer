cdf10f9dcaef16341822f622cc721d0754a05ea8
Wessel Stoop <wstoop@applejack.science.ru.nl>
2013-08-10 11:18:58 +0200

    Second attempt at making Soothsayer work out of the box

--- example_predicting.py
+++ example_predicting.py
@@ -3,22 +3,22 @@ import soothsayer
 ss = soothsayer.Soothsayer();
 
 #Load the language models
-pers_model = soothsayer.Languagemodel('allmail','w','d');
-bg_model = soothsayer.Languagemodel('nlsave','w','d');
-ss.start_servers([pers_model,bg_model],True);
+pers_model = soothsayer.Languagemodel('testinput','w','d');
+ss.start_servers([pers_model],True);
 
 #Load the lexicon
-lex = ss.load_lexicon('wordmodels/allmail.lex.txt');
+lex = ss.load_lexicon('wordmodels/testinput.lex.txt');
 
 #Set the module order
-ss.setup_basic_modules(pers_model);
+ss.modules = [soothsayer.Module(ss,'context-sensitive module',pers_model.name,'igtree'),
+              soothsayer.Module(ss,'context-insensitive module',pers_model.name,'lex'),]
 
 #Ask for prediction after every letter
 teststring = 'Dit is een zin om te testen';
 
 for n,i in enumerate(teststring):
     pred = ss.do_prediction(teststring[:n],lex,'');
-    print(pred['full_word'],pred['second_guess']);
+    print(teststring[:n], pred['full_word'],'(',pred['second_guess'],')');
 
 
 
@@ -26,32 +26,30 @@ for n,i in enumerate(teststring):
 
 
 ##OUTPUT:
-##  Using running server
-##  Using running server
-##_ Hoi
-##Dear Dag
-##Die Dit
-##Dit Ditmaal
-##zal is
-##is in
-##is isotoop
-##wat de
-##een eigenlijk
-##een eenvoudig
-##een eenvoudig
-##idee overzicht
-##zeer zogenaamde
-##zin zichzelf
-##zin zin
-##als van
-##op of
-##om omdat
-##het ze
-##te the
-##te tegen
-##kijken horen
-##testen the
-##testen tegen
-##testen testen
-##testen testen
-##testen
+## Dit ( is )
+##D Dit (  )
+##Di Dit (  )
+##Dit Dit (  )
+##Dit  is ( ook )
+##Dit i is (  )
+##Dit is is (  )
+##Dit is  een (  )
+##Dit is e een (  )
+##Dit is ee een (  )
+##Dit is een een (  )
+##Dit is een   (  )
+##Dit is een z  (  )
+##Dit is een zi  (  )
+##Dit is een zin  (  )
+##Dit is een zin  Dit ( is )
+##Dit is een zin o ook (  )
+##Dit is een zin om  (  )
+##Dit is een zin om  Dit ( is )
+##Dit is een zin om t testzin.Dit (  )
+##Dit is een zin om te testzin.Dit (  )
+##Dit is een zin om te  Dit ( is )
+##Dit is een zin om te t testzin.Dit (  )
+##Dit is een zin om te te testzin.Dit (  )
+##Dit is een zin om te tes testzin.Dit (  )
+##Dit is een zin om te test testzin.Dit (  )
+##Dit is een zin om te teste  (  )
